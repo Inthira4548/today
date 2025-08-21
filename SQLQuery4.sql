d1f2d0e720db707e29f53c6efe7c60ffdf167296
Inthira4548 <kineqwasg@gmail.com>
2025-08-21 09:38:50 +0700

    Add files via upload

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,15 +1,22 @@
+select  CategoryName,ProductName,UnitPrice
+from Products ,Categories 
+where Products.CategoryID = Categories.CategoryID
+
+Select  CategoryName,ProductName,UnitPrice
+from Products as P join Categories as C 
+on P.CategoryID = C.CategoryID
 
 
 
+select CompanyName,OrderID
+from Orders,Shippers 
+where Shippers.ShipperID = Orders.ShipVia
 
+select p.ProductID,p.ProductName,s.CompanyName,s.Country 
+from Products p , Suppliers s 
+where p.SupplierID = s.SupplierID
 
-
-
-
-
-
-
-
-
-
-
+select CompanyName,OrderID
+from Orders,Shippers 
+where Shippers.ShipperID = Orders.ShipVia
+and OrderID = 10275
